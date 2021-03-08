215d1e36842e1299b504f5e9d8c5c728c42dce2e
Tomas <tosanchez@frba.utn.edu.ar>
2021-03-08 14:36:23 +0000

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/App.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "profertil/realizarPagos/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("profertil.realizarPagos.controller.App", {});
+});
