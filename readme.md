a84f3805e52a83f97f2e1858b137c258b507cf70
Tomas <tosanchez@frba.utn.edu.ar>
2021-03-08 20:00:30 +0000

    ci: started home view

--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
-# Payments (or Realizar Pagos)
+# Interbanking
 
-Realizar Pagos allows customers to visualize their different payment methods.
+Interbanking allows customers to pay.
 
 See [Chengelog](./CHANGELOG.md).
 If you continue the development please, follow [standard-version](https://github.com/conventional-changelog/standard-version).
 
 ## Credits
 This project has been generated with 💙 and [easy-ui5](https://github.com/SAP)
-Initial release by [Tomas Sanchez](https://github.com/tomasanchez). @Softtek - Feb 2021.
+Initial release by [Tomas Sanchez](https://github.com/tomasanchez). @Softtek - March 2021.
--- uimodule/webapp/controller/Home.controller.js
+++ uimodule/webapp/controller/Home.controller.js
@@ -1,7 +1,86 @@
-sap.ui.define([
-  "profertil/realizarPagos/controller/BaseController"
-], function(Controller) {
-  "use strict";
+/**
+ * Home Controller.
+ *
+ * Methods of Home View.
+ *
+ * @file This files describes Home View controller.
+ * @author Tomas A. Sanchez
+ * @since 03.08.2021
+ */
+/* eslint-disable no-warning-comments */
+sap.ui.define(
+  [
+    "profertil/realizarPagos/controller/BaseController",
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter",
+  ],
+  function (Controller, JSONModel, formatter) {
+    "use strict";
+    // Bind this shortcut
+    var oController;
 
-  return Controller.extend("profertil.realizarPagos.controller.Home", {});
-});
+    return Controller.extend("profertil.realizarPagos.controller.Home", {
+      /**
+       * Formmater JS split-code.
+       * @memberOf com.profertil.view.Remitos
+       */
+      formatter: formatter,
+
+      /* =========================================================== */
+      /* lifecycle methods                                           */
+      /* =========================================================== */
+
+      /**
+       * Called when the Interbanking controller is instantiated.
+       * @memberOf profertil.view.Home
+       */
+      onInit: function () {
+        oController = this;
+
+        var oViewModel = new JSONModel({
+          showPaymentForm: true,
+          enablePayment: false,
+        });
+
+        oController.setModel(oViewModel, "homeView");
+
+        // Add the Interbanking page to the flp routing history
+        this.addHistoryEntry(
+          {
+            title: this.getResourceBundle().getText("title"),
+            icon: "sap-icon://simple-payment",
+            intent: "#Interbanking-display",
+          },
+          true
+        );
+      },
+      /**
+       * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+       * (NOT before the first rendering! onInit() is used for that one!).
+       * @memberOf com.profertil.view.Remitos
+       */
+      // onBeforeRendering: function () {
+      // },
+
+      /**
+       * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+       * This hook is the same one that SAPUI5 controls get after being rendered.
+       * @memberOf com.profertil.view.Remitos
+       */
+      //onAfterRendering: function () {
+      //},
+
+      /**
+       * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+       * @memberOf com.profertil.view.Remitos
+       */
+      //	onExit: function() {
+      //
+      //	}
+
+      /* =========================================================== */
+      /* event handlers                                              */
+      /* =========================================================== */
+    });
+  }
+);
